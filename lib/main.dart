4aa465f96557bae14dcedf100d5755373f930fcb
renceestoquee <dustinaron0@gmail.com>
2025-03-22 22:56:26 +0800

    functions

--- lib/main.dart
+++ lib/main.dart
@@ -30,6 +30,134 @@ class _MyAppState extends State<MyApp> {
   int currentGroup = 0;
   // VARIABLES END
 
+  // FUNCTION START
+  void addItemToList() {
+    if (itemController.text.isNotEmpty) {
+      setState(() {
+        items.add(itemController.text);
+        itemController.clear();
+      });
+    }
+  }
+
+  void generateGroups() {
+    int membersPerGroup = int.tryParse(groupController.text) ?? 0;
+
+    if (membersPerGroup > 0) {
+      showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          Future.delayed(Duration(seconds: 1), () {
+            Navigator.of(context).pop();
+          });
+
+          return CupertinoAlertDialog(
+            title: Text('Generating Groups'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 10),
+                CupertinoActivityIndicator(),
+                SizedBox(height: 10),
+                Text('Loading for generating random groups...'),
+              ],
+            ),
+          );
+        },
+      );
+
+      setState(() {
+        isGenerating = true;
+        groupController.text = '';
+        groupController.selection = TextSelection.collapsed(offset: 0);
+      });
+
+      items.shuffle();
+
+      groups = [];
+      int startIndex = 0;
+
+      while (startIndex < items.length) {
+        int endIndex = startIndex + membersPerGroup;
+        if (endIndex > items.length) endIndex = items.length;
+
+        List<String> group = items.sublist(startIndex, endIndex);
+        groups.add(group);
+        groupedItems.addAll(group);
+        startIndex = endIndex;
+      }
+
+      items = items.where((item) => !groupedItems.contains(item)).toList();
+
+      setState(() {
+        currentGroup = 0;
+      });
+    } else {
+      showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text("Invalid Members per Group"),
+            content: Text(
+              "The number of members per group must be greater than zero.",
+            ),
+            actions: [
+              CupertinoButton(
+                child: Text("OK"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
+
+  void showNextGroup() {
+    setState(() {
+      showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          Future.delayed(Duration(seconds: 1), () {
+            Navigator.of(context).pop();
+          });
+
+          return CupertinoAlertDialog(
+            title: Text('Generating Groups'),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 10),
+                CupertinoActivityIndicator(),
+                SizedBox(height: 10),
+                Text('Loading for generating random groups...'),
+              ],
+            ),
+          );
+        },
+      );
+
+      if (currentGroup < groups.length - 1) {
+        currentGroup++;
+      }
+    });
+  }
+
+  void reset() {
+    setState(() {
+      items.clear();
+      groups.clear();
+      groupedItems.clear();
+      currentGroup = 0;
+      isGenerating = false;
+      groupController.clear();
+    });
+  }
+
+  // FUNCTION END
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
